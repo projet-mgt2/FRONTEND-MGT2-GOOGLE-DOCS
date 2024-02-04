a249c6fb310fd285ce58b2a810aec77118d1e9b2
Maharavo1 <hei.maharavo@gmail.com>
2024-02-04 18:45:10 +0300

    feat : Set up Home component with title and login form

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 
 import Head from "next/head";
+import Login from "./login";
 export default function Home() {
   return (
     <div>
     <Head>
       <title>Google Docs</title>
     </Head>
+    <Login/>
     </div>
   )
 }
