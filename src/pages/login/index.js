04cfcfef9442867948b4e63c93d70a58e88b7a12
Maharavo1 <hei.maharavo@gmail.com>
2024-02-07 17:13:45 +0300

    fix : change design of the login page

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 import { useRouter } from 'next/router';
 import { useForm } from "react-hook-form";
 import apiUser from '../../api/user';
-import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+import { faEye, faEyeSlash , faFileAlt } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 
@@ -12,7 +12,7 @@ export default function Login() {
   const [showPassword, setShowPassword] = useState(false);
 
   const router = useRouter();
-  const [bg, setBg] = useState("bg-white p-8 rounded-md w-96 drop-shadow-2xl");
+  const [bg, setBg] = useState(" p-8 w-120");
 
   const handleSignupClick = () => {
     router.push('/signup');
@@ -21,78 +21,94 @@ export default function Login() {
   const onSubmit = async (data) => {
     apiUser.login(data).then((res) => {
       if (res == "Authentification with succes") {
-        setBg("bg-white p-8 rounded-md w-96 drop-shadow-[0_25px_25px_rgba(0,255,0,0.5)]");
+        setBg("bg-white   w-120 drop-shadow-[0_25px_25px_rgba(0,255,0,0.5)]");
         setTimeout(() => {
           router.push('/home');
         }, 1000);
       } else {
-        setBg("bg-white p-8 rounded-md w-96 drop-shadow-[0_25px_25px_rgba(255,0,0,0.5)]");
+        setBg("bg-white rounded-md w-120 drop-shadow-[0_25px_25px_rgba(255,0,0,0.5)]");
         setTimeout(() => {
-          setBg("bg-white p-8 rounded-md w-96 drop-shadow-2xl");
-        }, 1000);
+          setBg("bg-white  rounded-md w-120 drop-shadow-2xl");
+          alert("Username or password not valid")
+        }, 100);
       }
     });
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-white">
+    <div className="min-h-screen flex items-center  justify-center bg-blue-100">
       <div className={bg}>
-        <h2 className="text-2xl text-black font-semibold mb-6">Login from google docs</h2>
+        <div className='flex w-160 bg-white rounded-md' >
+        <div className="flex flex-col drop-shadow-2xl items-center w-96 mt-12 p-8 rounded-l-md">
+          <FontAwesomeIcon
+          icon={faFileAlt}
+          size="6x"
+          style={{ color: "blue" }}
+         />
+          <h2 className="text-xl text-black font-semibold  mt-10">Google Docs</h2>
 
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <div className="mb-4">
-            <label htmlFor="username" className="block text-sm font-medium text-gray-600">
-              Username
-            </label>
-            <input
-              type="text"
-              id="username"
-              name="username"
-              className="mt-1 p-2 border border-gray-300 w-full rounded-md text-black"
-              {...register("username", {required: true})}
-            />
-          </div>
-          <div className="mb-4 flex flex-col">
-            <label htmlFor="password" className="block text-sm font-medium text-gray-600">
-              Password
-            </label>
-            <div className="relative flex items-center">
-              <input
-                type={showPassword ? 'text' : 'password'}
-                id="password"
-                name="password"
-                className="mt-1 p-2 border border-gray-300 w-full rounded-md text-black"
-                {...register("password", { required: true })}
-              />
-              <FontAwesomeIcon
-                icon={showPassword ? faEyeSlash : faEye}
-                className="absolute top-2 right-3 cursor-pointer text-gray-500 mt-2"
-                onClick={() => setShowPassword(!showPassword)}
-              />
-            </div>
-          </div>
+        </div>
+        <div className='p-8 w-96'>
+      
+            <h2 className="text-2xl text-black font-semibold mb-6 text-center bg-red">Login</h2>
+          
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <div className="mb-4">
+                <label htmlFor="username" className="block text-sm font-medium text-gray-600">
+                  Username
+                </label>
+                <input
+                  type="text"
+                  id="username"
+                  name="username"
+                  className="mt-1 p-2 border border-gray-300 w-full  text-black"
+                  {...register("username", {required: true})}
+                />
+              </div>
+              <div className="mb-4 flex flex-col">
+                <label htmlFor="password" className="block text-sm font-medium text-gray-600">
+                  Password
+                </label>
+                <div className="relative flex items-center">
+                  <input
+                    type={showPassword ? 'text' : 'password'}
+                    id="password"
+                    name="password"
+                    className="mt-1 p-2 border border-gray-300 w-full  text-black"
+                    {...register("password", { required: true })}
+                  />
+                  <FontAwesomeIcon
+                    icon={showPassword ? faEyeSlash : faEye}
+                    className="absolute top-2 right-3 cursor-pointer text-gray-500 mt-2"
+                    onClick={() => setShowPassword(!showPassword)}
+                  />
+                </div>
+              </div>
 
- 
-          <button
-            type="submit"
-            className="w-full bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600"
-          >
-            Login
-          </button>
-        </form>
+    
+              <button
+                type="submit"
+                className="w-full bg-blue-500 text-white py-2  hover:bg-blue-600"
+              >
+                Login
+              </button>
+            </form>
 
+          
+            <p className="mt-2 text-sm text-gray-600">
+              <a href="/forgot-password">Don't have an account?</a>
+            </p>
+
+            <button
+              type="button"
+              onClick={handleSignupClick}
+              className="w-full mt-4 bg-green-500 text-white py-2  hover:bg-green-600"
+            >
+              Signup
+            </button>
+      </div>
+        </div>
        
-        <p className="mt-2 text-sm text-gray-600">
-          <a href="/forgot-password">Forgot Password?</a>
-        </p>
-
-        <button
-          type="button"
-          onClick={handleSignupClick}
-          className="w-full mt-4 bg-green-500 text-white py-2 rounded-md hover:bg-green-600"
-        >
-          Signup
-        </button>
       </div>
     </div>
   );
