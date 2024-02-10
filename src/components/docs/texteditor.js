89aed7651cdc2fd78b7d840ac4c835c90271f1a4
Maharavo1 <hei.maharavo@gmail.com>
2024-02-10 10:38:42 +0300

    fix: update texteditor.js to make all text styles functional

--- src/components/docs/texteditor.js
+++ src/components/docs/texteditor.js
@@ -2,23 +2,21 @@ import React, { useState } from 'react';
 import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 import dynamic from 'next/dynamic';
 
-const DynamicEditor = dynamic(
-    () => import('react-draft-wysiwyg').then((module) => module.Editor),
-    {
-        ssr: false,
-    }
+const Editor = dynamic(
+  () => import("react-draft-wysiwyg").then((module) => module.Editor),
+  {
+    ssr: false,
+  }
 );
 
 export default function TextEditor() {
    
-    return (
-        
-        <div className='bg-[#F8F9FA] min-h-screen pb-16 text-black'>
-            <DynamicEditor
-                
-                toolbarClassName='flex sticky top-0 z-50 !justify-center mx-auto'
-                editorClassName='mt-6 bg-white shadow-lg min-h-[120vh] max-h-[120vh] max-w-5xl mx-auto'
-            />
-        </div>
-    );
+     return (
+    <div className="bg-[#F1F3F4] dark:bg-dark-extra min-h-screen pb-16 print:pb-0 ">
+      <Editor
+        toolbarClassName="flex sticky top-0 z-50 sm:!justify-center print:!hidden dark:!bg-dark-mid !border-0"
+        editorClassName="mt-4 print:!m-0 print:!mx-auto sm:mt-6 py-1 sm:py-5 px-4 sm:px-10 bg-white shadow-md print:!shadow-none max-w-[90%] sm:max-w-3xl !min-h-[1024px] mx-auto ring-1 print:ring-0 ring-gray-300 print:block"
+      />
+    </div>
+  );
 }
